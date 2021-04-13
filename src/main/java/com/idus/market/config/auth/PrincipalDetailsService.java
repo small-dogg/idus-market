34502b197dbd251c1039c535c7662c9a78f1e78b
small-dogg <74956749+small-dogg@users.noreply.github.com>
2021-04-13 17:14:11 +0900

    fix: loadUserByUsername with email in PrincipalDetail

--- src/main/java/com/idus/market/config/auth/PrincipalDetailsService.java
+++ src/main/java/com/idus/market/config/auth/PrincipalDetailsService.java
@@ -15,8 +15,8 @@ public class PrincipalDetailsService implements UserDetailsService {
   private final UserRepository userRepository;
 
   @Override
-  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userRepository.findByUsername(username).get();
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(email).get();
     if (user == null) {
       throw new UsernameNotFoundException("Invalid username or password.");
     }
