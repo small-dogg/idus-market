47adc76d91b9b97fb366e00151a882d8cbe1dfbd
sypark <sypark@innogrid.com>
2021-04-16 15:10:53 +0900

    fix: make Read-only the Orders Service

--- src/main/java/com/idus/market/service/OrdersService.java
+++ src/main/java/com/idus/market/service/OrdersService.java
@@ -9,9 +9,11 @@ import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class OrdersService {
 
   private final OrdersRepository ordersRepository;
